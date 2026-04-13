ac6b3497d6389300d291be167702883dad08200b
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-04-13 12:50:29 +0530

    Old way to handle different URL verbs

--- school-go-api/cmd/api/server.go
+++ school-go-api/cmd/api/server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
@@ -32,75 +30,6 @@ func main() {
 			fmt.Println("Hello GET method on teachers route")
 			return
 		case http.MethodPost:
-
-			// Parse the form data (necessary for x-www-form-urlencoded)
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, "Error parsing form", http.StatusBadRequest)
-				return
-			}
-
-			fmt.Println("Form:", r.Form)
-
-			// Prepare response data
-			response := make(map[string]any)
-			for k, v := range r.Form {
-				response[k] = v[0]
-			}
-			fmt.Println("Processed Response Map:", response)
-
-			// RAW BODY
-
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				return
-			}
-			defer r.Body.Close()
-
-			fmt.Println("Raw body:", string(body))
-
-			var userInstance user
-
-			err = json.Unmarshal(body, &userInstance)
-			if err != nil {
-				http.Error(w, fmt.Sprintf("Error while marshlling %s", err), http.StatusBadRequest)
-				return
-			}
-
-			fmt.Println("Unmarshalled json into user instance", userInstance)
-			fmt.Println("Recieved User name as:", userInstance.Name)
-
-			// Prepare response data
-			response1 := make(map[string]any)
-			for k, v := range r.Form {
-				response[k] = v[0]
-			}
-
-			err = json.Unmarshal(body, &response1)
-			if err != nil {
-				http.Error(w, "Error while marshlling", http.StatusBadRequest)
-				return
-			}
-			fmt.Println("Unmarshalled json into a map", response1)
-
-			fmt.Println("Body:", r.Body)
-			fmt.Println("Form:", r.Form)
-			fmt.Println("Header:", r.Header)
-			fmt.Println("Context:", r.Context())
-			fmt.Println("Context Length:", r.ContentLength)
-			fmt.Println("Host:", r.Host)
-			fmt.Println("Method:", r.Method)
-			fmt.Println("Protocol:", r.Proto)
-			fmt.Println("Remote Addr:", r.RemoteAddr)
-			fmt.Println("Request URI:", r.RequestURI)
-			fmt.Println("TLS:", r.TLS)
-			fmt.Println("Trailer:", r.Trailer)
-			fmt.Println("Transfer Encoding:", r.TransferEncoding)
-			fmt.Println("URL:", r.URL)
-			fmt.Println("UserAgent:", r.UserAgent())
-			fmt.Println("Port:", r.URL.Port())
-			fmt.Println("Scheme:", r.URL.Scheme)
-
 			w.Write([]byte("Hello POST method on teachers route"))
 			fmt.Println("Hello POST s method on teachers route")
 			return
@@ -125,16 +54,62 @@ func main() {
 	// Students Route
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hello Root Route")
-		w.Write([]byte("Hello students route"))
-		fmt.Println("Hello students route")
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Hello GET method on Students route"))
+			fmt.Println("Hello GET method on Students route")
+			return
+		case http.MethodPost:
+			w.Write([]byte("Hello POST method on Students route"))
+			fmt.Println("Hello POST s method on Students route")
+			return
+		case http.MethodPut:
+			w.Write([]byte("Hello PUT method on Students route"))
+			fmt.Println("Hello PUT method on Students route")
+			return
+		case http.MethodPatch:
+			w.Write([]byte("Hello PATCH method on Students route"))
+			fmt.Println("Hello PATCH method on Students route")
+			return
+		case http.MethodDelete:
+			w.Write([]byte("Hello DELETE method on Students route"))
+			fmt.Println("Hello DELETE method on Students route")
+			return
+		default:
+		}
+		w.Write([]byte("Hello Students route"))
+		fmt.Println("Hello Students route")
 
 	})
 
 	// EXECS Route
 	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "Hello Root Route")
-		w.Write([]byte("Hello execs route"))
-		fmt.Println("Hello execs route")
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Hello GET method on Execs route"))
+			fmt.Println("Hello GET method on Execs route")
+			return
+		case http.MethodPost:
+			w.Write([]byte("Hello POST method on Execs route"))
+			fmt.Println("Hello POST s method on Execs route")
+			return
+		case http.MethodPut:
+			w.Write([]byte("Hello PUT method on Execs route"))
+			fmt.Println("Hello PUT method on Execs route")
+			return
+		case http.MethodPatch:
+			w.Write([]byte("Hello PATCH method on Execs route"))
+			fmt.Println("Hello PATCH method on Execs route")
+			return
+		case http.MethodDelete:
+			w.Write([]byte("Hello DELETE method on Execs route"))
+			fmt.Println("Hello DELETE method on Execs route")
+			return
+		default:
+		}
+		w.Write([]byte("Hello Execs route"))
+		fmt.Println("Hello Execs route")
 
 	})
 
