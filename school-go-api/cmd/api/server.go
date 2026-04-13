6e19740e95d45026e755eb644dd1fd112bea2eee
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-04-13 13:02:55 +0530

    Extracted the handlers out of the main function

--- school-go-api/cmd/api/server.go
+++ school-go-api/cmd/api/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type user struct {
@@ -12,106 +13,117 @@ type user struct {
 	City string `json:"city"`
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello Root Route")
+	w.Write([]byte("Hello root route"))
+	fmt.Println("Hello root route")
+}
+
+func teachersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Println(r.URL.Path)
+		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
+		userId := strings.TrimSuffix(path, "/")
+		fmt.Println("The id is:", userId)
+		w.Write([]byte("Hello GET method on teachers route"))
+		fmt.Println("Hello GET method on teachers route")
+		return
+	case http.MethodPost:
+		w.Write([]byte("Hello POST method on teachers route"))
+		fmt.Println("Hello POST s method on teachers route")
+		return
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT method on teachers route"))
+		fmt.Println("Hello PUT method on teachers route")
+		return
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH method on teachers route"))
+		fmt.Println("Hello PATCH method on teachers route")
+		return
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE method on teachers route"))
+		fmt.Println("Hello DELETE method on teachers route")
+		return
+	default:
+		w.Write([]byte("Hello teachers route"))
+		fmt.Println("Hello teachers route")
+	}
+}
+
+func studentsHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello Root Route")
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Hello GET method on Students route"))
+		fmt.Println("Hello GET method on Students route")
+		return
+	case http.MethodPost:
+		w.Write([]byte("Hello POST method on Students route"))
+		fmt.Println("Hello POST s method on Students route")
+		return
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT method on Students route"))
+		fmt.Println("Hello PUT method on Students route")
+		return
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH method on Students route"))
+		fmt.Println("Hello PATCH method on Students route")
+		return
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE method on Students route"))
+		fmt.Println("Hello DELETE method on Students route")
+		return
+	default:
+	}
+	w.Write([]byte("Hello Students route"))
+	fmt.Println("Hello Students route")
+
+}
+
+func execsHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello Root Route")
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("Hello GET method on Execs route"))
+		fmt.Println("Hello GET method on Execs route")
+		return
+	case http.MethodPost:
+		w.Write([]byte("Hello POST method on Execs route"))
+		fmt.Println("Hello POST s method on Execs route")
+		return
+	case http.MethodPut:
+		w.Write([]byte("Hello PUT method on Execs route"))
+		fmt.Println("Hello PUT method on Execs route")
+		return
+	case http.MethodPatch:
+		w.Write([]byte("Hello PATCH method on Execs route"))
+		fmt.Println("Hello PATCH method on Execs route")
+		return
+	case http.MethodDelete:
+		w.Write([]byte("Hello DELETE method on Execs route"))
+		fmt.Println("Hello DELETE method on Execs route")
+		return
+	default:
+	}
+	w.Write([]byte("Hello Execs route"))
+	fmt.Println("Hello Execs route")
+
+}
 func main() {
 	port := ":3000"
 
 	// Root Route
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Hello Root Route")
-		w.Write([]byte("Hello root route"))
-		fmt.Println("Hello root route")
-	})
+	http.HandleFunc("/", rootHandler)
 
 	// Teachers Route
-	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET method on teachers route"))
-			fmt.Println("Hello GET method on teachers route")
-			return
-		case http.MethodPost:
-			w.Write([]byte("Hello POST method on teachers route"))
-			fmt.Println("Hello POST s method on teachers route")
-			return
-		case http.MethodPut:
-			w.Write([]byte("Hello PUT method on teachers route"))
-			fmt.Println("Hello PUT method on teachers route")
-			return
-		case http.MethodPatch:
-			w.Write([]byte("Hello PATCH method on teachers route"))
-			fmt.Println("Hello PATCH method on teachers route")
-			return
-		case http.MethodDelete:
-			w.Write([]byte("Hello DELETE method on teachers route"))
-			fmt.Println("Hello DELETE method on teachers route")
-			return
-		default:
-			w.Write([]byte("Hello teachers route"))
-			fmt.Println("Hello teachers route")
-		}
-	})
+	http.HandleFunc("/teachers/", teachersHandler)
 
 	// Students Route
-	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Hello Root Route")
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET method on Students route"))
-			fmt.Println("Hello GET method on Students route")
-			return
-		case http.MethodPost:
-			w.Write([]byte("Hello POST method on Students route"))
-			fmt.Println("Hello POST s method on Students route")
-			return
-		case http.MethodPut:
-			w.Write([]byte("Hello PUT method on Students route"))
-			fmt.Println("Hello PUT method on Students route")
-			return
-		case http.MethodPatch:
-			w.Write([]byte("Hello PATCH method on Students route"))
-			fmt.Println("Hello PATCH method on Students route")
-			return
-		case http.MethodDelete:
-			w.Write([]byte("Hello DELETE method on Students route"))
-			fmt.Println("Hello DELETE method on Students route")
-			return
-		default:
-		}
-		w.Write([]byte("Hello Students route"))
-		fmt.Println("Hello Students route")
-
-	})
+	http.HandleFunc("/students/", studentsHandler)
 
 	// EXECS Route
-	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Hello Root Route")
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET method on Execs route"))
-			fmt.Println("Hello GET method on Execs route")
-			return
-		case http.MethodPost:
-			w.Write([]byte("Hello POST method on Execs route"))
-			fmt.Println("Hello POST s method on Execs route")
-			return
-		case http.MethodPut:
-			w.Write([]byte("Hello PUT method on Execs route"))
-			fmt.Println("Hello PUT method on Execs route")
-			return
-		case http.MethodPatch:
-			w.Write([]byte("Hello PATCH method on Execs route"))
-			fmt.Println("Hello PATCH method on Execs route")
-			return
-		case http.MethodDelete:
-			w.Write([]byte("Hello DELETE method on Execs route"))
-			fmt.Println("Hello DELETE method on Execs route")
-			return
-		default:
-		}
-		w.Write([]byte("Hello Execs route"))
-		fmt.Println("Hello Execs route")
-
-	})
+	http.HandleFunc("/execs/", execsHandler)
 
 	fmt.Println("Server is running on port:", port)
 	err := http.ListenAndServe(port, nil)
